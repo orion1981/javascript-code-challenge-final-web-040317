c92c253ba71a61490d5c0efe6ba8fd15b352ba49
github username <github email address>
2017-06-02 10:35:07 -0400

    pencils down

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
+
   // your code here!
+
+    $("#photo-form").on("submit", function(event){
+      event.preventDefault()
+      console.log("form submitted")
+      let urlImg = $("#image_url").val()
+      let caption = $("#caption").val()
+      let imgCap = `<h2> ${caption}</h2>`
+      let picture = `<img src="${urlImg}" >`
+
+      // let addComment =
+      // let comments = `<ul> ${comments.render()} </ul>`
+
+      $("#photo-list").append(picture)
+      $("#photo-list h1").append(caption)
+
+  })
+
 })
---
+++ src/photo.js
@@ -0,0 +1,10 @@
+class Photo{
+  constructor(url, caption){
+    this.url = url
+    this.caption = caption
+    this.comments = []
+  }
+
+  render()
+  
+}
